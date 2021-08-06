f826469a8286e07a9e832cd0079708c28d882166
emili <emili@MSI>
2021-08-05 23:04:33 -0600

    Agrego una clase posicion que me ayudara a la identificar de la cadena

---
+++ src/Objetos/Posicion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author emili
+ */
+public class Posicion {
+    
+    private int posicion;
+
+    public Posicion(int posicon) {
+        this.posicion = posicon;
+    }
+
+    public int getPosicon() {
+        return posicion;
+    }
+}
